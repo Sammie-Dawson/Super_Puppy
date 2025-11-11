61de08d963d3f2d3847640c0f6104f9996388916
Sammie Dawson <sxd230190@utdallas.edu>
2025-11-11 14:12:52 -0600

    src folder created

--- project.py
+++
@@ -1,5 +0,0 @@
-#scare def - bunnies in the same y cordinates as supper puppy are removed and added to to counter bunnines_gone(#)
-#sonic_bark(x,y cordinates of puppy) (when up arrow pushed) calls image/image sequnce to play from puppy and sends x,y to scare(x,y)
-# bunnies_gone- keeps running track of bunnies scared resets at level completion 
-#
-#class- bunnies 
---
+++ src/project.py
@@ -0,0 +1,5 @@
+#scare def - bunnies in the same y cordinates as supper puppy are removed and added to to counter bunnines_gone(#)
+#sonic_bark(x,y cordinates of puppy) (when up arrow pushed) calls image/image sequnce to play from puppy and sends x,y to scare(x,y)
+# bunnies_gone- keeps running track of bunnies scared resets at level completion 
+#
+#class- bunnies 
