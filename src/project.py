f40f9470736860dc260a271d6d8222133516fb6f
Sammie-Dawson <sxd230190@utdallas.edu>
2025-11-11 17:52:46 -0600

    planing

--- src/project.py
+++ src/project.py
@@ -1,5 +1,13 @@
-#scare def - bunnies in the same y cordinates as supper puppy are removed and added to to counter bunnines_gone(#)
 #sonic_bark(x,y cordinates of puppy) (when up arrow pushed) calls image/image sequnce to play from puppy and sends x,y to scare(x,y)
 # bunnies_gone- keeps running track of bunnies scared resets at level completion 
 #
-#class- bunnies 
+#class- bunnies (level up, sonic_bark)
+        #def self
+               # level up reset
+               # kill counter
+               # sonic bark cordinates
+               # 
+        #image open turn to sprite (self)
+        #kill switch(if sonic bark or level up)
+        #scare def - bunnies in the same y cordinates as supper puppy are removed and added to to counter bunnines_gone(#)
+        #
