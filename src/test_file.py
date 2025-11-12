12fcac9b271ec9603ae85730dc82c68cd0ffa19e
Sammie-Dawson <sxd230190@utdallas.edu>
2025-11-11 19:28:27 -0600

    no movemetn almonst therre

--- src/test_file.py
+++ src/test_file.py
@@ -27,13 +27,23 @@ def mouse_resaponse(screen):
         YELLOW=pygame.Color(186, 155, 89)
         circle=pygame.draw.circle(ball, (YELLOW), (50,50), (radius))
         screen.blit(ball, (x_b, y_b))
+        return  x_b, y_b 
         
-
+def image(screen, fullscreen):
+    background = pygame.image.load('Supper_puppy.png').convert_alpha()
+    if fullscreen==True:
+        sizes=pygame.display.get_desktop_sizes()
+    else:
+        sizes=[(800,600)]
+    Width=screenSize_Format_Width(sizes)
+    height=screenSize_Format_Height(sizes)
+    NameTag=screen.blit(background, ((Width//3), (height-200)))
 
 def move_left(cordinates):
     x_cordinate, y_cordinate=cordinates
-    new_cordinates=(x_cordinate +1), (y_cordinate)
+    new_cordinates=((x_cordinate +10), (y_cordinate))
     result=new_cordinates
+    print (result)
     return result
 
 def move_right(cordinates):
@@ -46,15 +56,15 @@ def bunnies():
     x=5
 class supper_puppy():
 
-    def __init__(self, pos=(1, 0), size=30, life=1000):
+    def __init__(self, position=(0, 0), size=30, life=1000):
         self.size = size
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]: # up key
-            position= move_left(pos)
+            position= move_left(position)
         elif key[pygame.K_RIGHT]: # up key
-            position=move_right(pos)
+            position=move_right(position)
         else:
-            position=pos
+            position=position
         self.pos = position
         self.age = 0 # in milliseconds
         self.life = life # in milliseconds
@@ -69,22 +79,29 @@ class supper_puppy():
             self.dead = True
         self.alpha = 255 * (1 - (self.age / self.life))
     
-    def image_run(self):
+    #def image_run():
         
-        background = pygame.image.load('Supper_puppy.png').convert_alpha()
+       # background = pygame.image.load('Supper_puppy.png').convert_alpha()
 
       
 
     def update_surface(self):
-        dog = supper_puppy.image_run()
-        NameTag=self.surface.blit(dog, (self.pos))
+        dog = pygame.image.load('Supper_puppy.png').convert_alpha()
         return dog
     
     def draw(self, surface):
-        if self.dead:
-            return
-        self.surface.set_alpha(self.alpha)
         surface.blit(self.surface, self.pos)
+
+        x_b, y_b = pygame.mouse.get_pos()
+        print(self.pos)
+   
+        #surface.blit(self.surface, (x_b, y_b))
+       # if self.dead:
+         #      return
+        #self.surface.set_alpha(self.alpha)
+       
+        
+
 
 
 def main():
@@ -95,7 +112,8 @@ def main():
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
     #Bunnies = bunnies(resolution)
-    Supper_Pupppy=supper_puppy(resolution)
+    fix_dog = (400, 400)
+    Supper_Pupppy=supper_puppy(fix_dog)
     running = True
     fullscreen=False
     while running:
@@ -106,21 +124,24 @@ def main():
                 sizes=pygame.display.get_desktop_sizes()
                 Width_screen=screenSize_Format_Width(sizes)
                 fullscreen=True
-        
         if fullscreen==True:
-            Supper_Pupppy.update(dt,Width_screen,fullscreen)
+            Supper_Pupppy.update(dt)
+            #Supper_Pupppy.update(dt,Width_screen,fullscreen)
             #bunnies.update(dt,Width_screen,fullscreen)
         else:  
             width_normal=800
-            supper_puppy.update(dt, width_normal, False)
+            Supper_Pupppy.update(dt)
+            #Supper_Pupppy.update(dt, width_normal, False)
             #bunnies.update(dt,width_normal,fullscreen)
-        black = pygame.Color(0, 0, 0)
+        black = pygame.Color(255, 255, 255)
         screen.fill(black)
         #bunnies.draw(screen)
+        image(screen, fullscreen)
+
         Supper_Pupppy.draw(screen)
         mouse_resaponse(screen)
         pygame.display.flip()
-        dt = clock.tick(6)
+        dt = clock.tick(12)
     pygame.quit()
 
 if __name__ == "__main__":
