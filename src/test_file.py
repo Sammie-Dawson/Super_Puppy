71133a3185dbf0b845b65984ec26ca3d3cb8507e
Sammie-Dawson <sxd230190@utdallas.edu>
2025-12-05 16:29:18 -0600

    level 3 bunnnies

--- src/test_file.py
+++ src/test_file.py
@@ -19,6 +19,15 @@ class bunnies():
         background = pygame.image.load('level3_Bunny_small_hide.png').convert_alpha()
 
         NameTag=screen.blit(background, (bunny))
+
+
+
+    def level3_bunny_print_big_hide(screen, bunny):
+        background = pygame.image.load('level3_Bunny_big_hide.png').convert_alpha()
+
+        NameTag=screen.blit(background, (bunny))
+
+
     def level_system(level,screen):
         if level==1:
             bunny_1a=((0),(160))
@@ -35,15 +44,13 @@ class bunnies():
             bunny_no_hide=[(bunny_1b),(bunny_1c),(bunny_2a),(bunny_2c),(bunny_3a),(bunny_3b),(bunny_3c)]
             for bunny in bunny_no_hide:
                 bunnies.bunny_print_no_hide(screen,bunny)
-        elif level==2:
+        elif level==3:
             bunny_1a=((0),(160))
             bunny_1b=((133),(160))
             bunny_1c=((266),(160))
             bunny_1d=((399),(160))
             bunny_1e=((534),(160))
             bunny_1f=((667),(160))
-
-
 
             bunny_2a=((0),(270))
             bunny_2b=((133),(270))
@@ -59,13 +66,16 @@ class bunnies():
             bunny_3e=((534),(380))
             bunny_3f=((667),(380))
 
-            bunny_no_hide=[bunny_1a,bunny_2c,bunny_3b]
+            bunny_no_hide=[bunny_1a,bunny_1f,bunny_2f,bunny_3c]
             for bunny in bunny_no_hide:
-                bunnies.bunny_print_no_hide(screen,bunny)
-            bunny_small_hide=(bunny_1b),(bunny_1c),(bunny_2a),(bunny_2b),(bunny_3a),(bunny_3c)
+                bunnies.level3_bunny_print_no_hide(screen,bunny)
+            bunny_small_hide=(bunny_1b),(bunny_1c),(bunny_1e),(bunny_2b),(bunny_2d),(bunny_3a),(bunny_3e)
             for bunny in bunny_small_hide:
-                bunnies.bunny_print_small_hide(screen,bunny)
-        elif level==3:
+                bunnies.level3_bunny_print_small_hide(screen,bunny)
+            bunny_big_hide=[bunny_1d,bunny_2a,bunny_2c,bunny_2e,bunny_3b,bunny_3d,bunny_3f]
+            for bunny in bunny_big_hide:
+                bunnies.level3_bunny_print_big_hide(screen,bunny)
+        elif level==2:
             bunny_1a=((0),(160))
             bunny_1b=((266),(160))
             bunny_1c=((534),(160))
@@ -79,10 +89,10 @@ class bunnies():
             bunny_3c=((534),(380))
             bunny_no_hide=[bunny_1a,bunny_2c,bunny_3b]
             for bunny in bunny_no_hide:
-                bunnies.level3_bunny_print_no_hide(screen,bunny)
+                bunnies.bunny_print_no_hide(screen,bunny)
             bunny_small_hide=(bunny_1b),(bunny_1c),(bunny_2a),(bunny_2b),(bunny_3a),(bunny_3c)
             for bunny in bunny_small_hide:
-                bunnies.level3_bunny_print_small_hide(screen,bunny)
+                bunnies.bunny_print_small_hide(screen,bunny)
 
 
 def Resize():
